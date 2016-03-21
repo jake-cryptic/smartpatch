742095be5a4ee306a67e32afc5e993ae64e5c0e0
Jake Mcneill <jake-cryptic@users.noreply.github.com>
2016-03-21 11:14:51 +0000

    Update upload.php
    Hopefully fix file upload bug

--- assets/php/scripts/upload.php
+++ assets/php/scripts/upload.php
@@ -5,27 +5,27 @@ if (accountLoggedIn()) { // Only if account is logged in
 	// Check if they can upload a patch
 	if (checkPatchAmount()) {
 		// Did they upload the file
-		if($_FILES['fileToUpload']['name']) {
+		if(@$_FILES['theFile']['name']) {
 			
 			// Check for errors
-			if(!$_FILES['fileToUpload']['error']) {
+			if(!$_FILES['theFile']['error']) {
 				
 				// Set the file name for later use
 				$currentdir = getcwd();
-				if ($usingLinux == false) {
-					$target = $currentdir . '\..\..\..\patch\\' . $_SESSION["username"] . '\\' . basename($_FILES['fileToUpload']['name']);
-					$destination = $currentdir . '\..\..\..\patch\\' . $_SESSION["username"] . '\\';
-				} else {
-					$target = $currentdir . '/../../../patch/' . $_SESSION["username"] . '/' . basename($_FILES['fileToUpload']['name']);
-					$destination = $currentdir . '/../../../patch/' . $_SESSION["username"] . '/';
+				if (strpos($currentdir,"\\") !== false) {
+					$target = $currentdir . '\..\..\..\patch\\' . stripslashes($_SESSION["username"]) . '\\' . basename($_FILES['theFile']['name']);
+					$destination = $currentdir . '\..\..\..\patch\\' . stripslashes($_SESSION["username"]) . '\\';
+				} else{
+					$target = $currentdir . '/../../../patch/' . stripslashes($_SESSION["username"]) . '/' . basename($_FILES['theFile']['name']);
+					$destination = $currentdir . '/../../../patch/' . stripslashes($_SESSION["username"]) . '/';
 				}
 				
 				// Check the file is a zip archive
-				$explodedFileName = explode(".",strtolower($_FILES["fileToUpload"]["name"]));
+				$explodedFileName = explode(".",strtolower($_FILES["theFile"]["name"]));
 				$fileExtension = end($explodedFileName);
 				if ($fileExtension == "zip") {
-					$new_file_name = strtolower($_FILES['fileToUpload']['tmp_name']); // Rename the file
-					if($_FILES['fileToUpload']['size'] > ($_SESSION["allowedPatchSize"])) { // Must be smaller than is allowed for accountType 
+					$new_file_name = strtolower($_FILES['theFile']['tmp_name']); // Rename the file
+					if($_FILES['theFile']['size'] > ($_SESSION["allowedPatchSize"])) { // Must be smaller than is allowed for accountType 
 						$valid_file = false;
 					} else { 
 						$valid_file = true;
@@ -35,41 +35,35 @@ if (accountLoggedIn()) { // Only if account is logged in
 					if($valid_file) {
 						if (file_exists($target)) {
 							if (!unlink($target)) {
-								header("Location: $fullPathToRoot?p=upl&msg=1");
-								die("Redirecting");
+								die('<span class="statusMsgErr">File already exists, please contact support for help</span>');
 							}
 						}
-						if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target)) {
+						if (move_uploaded_file($_FILES['theFile']['tmp_name'], $target)) {
 							$_SESSION["fileUploaded"] = $target; // Set so unzip.php knows what to target
 							$_SESSION["fileDestination"] = $destination; // Set so unzip.php knows other stuff
 							header("Location: unzip.php");
 							die("Redirecting");
 						} else {
-							header("Location: $fullPathToRoot?p=upl&msg=4");
-							die("Redirecting");
+							die('<span class="statusMsgErr">Files couldn\'t be moved, contact support</span>');
 						}
 					} else {
-						header("Location: $fullPathToRoot?p=upl&msg=2");
-						die("Redirecting");
+						die('<span class="statusMsgErr">This patch breaks our file size limit of ' . $_SESSION["allowedPatchSizeStr"] . '</span>');
 					}
 				} else {
-					header("Location: $fullPathToRoot?p=upl&msg=3");
-					die("Redirecting");
+					die('<span class="statusMsgErr">The file uploaded was not in .zip format!</span>');
 				}
 			} else {
 				// Any more problems?
-				header("Location: $fullPathToRoot?p=upl&msg=6");
-				die("Redirecting");
+				die('<span class="statusMsgErr">General file upload error, contact support</span>');
 			}
 		} else {
-			die("I don't know what your trying to upload but it ain't a file");
+			die('<span class="statusMsgErr">Sorry, We only accept files</span>');
 		}
 	} else {
-		header("Location: $fullPathToRoot?p=upl&msg=10");
-		die("Redirecting");
+		die('<span class="statusMsgErr">We have a limit of ' . $_SESSION["allowedPatchInt"] . ' patches</span>');
 	}
-} else { 
+} else {
 	header("Location: $fullPathToRoot");
-	die("You aren't logged in");
+	die('You are\'nt logged in</span>');
 }
 ?>
